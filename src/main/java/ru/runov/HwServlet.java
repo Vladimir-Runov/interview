4afaabc83aaf28d1cf16060d30a8eae047540102
Vladimir-Runov <9322304@gmail.com>
2021-12-08 00:04:12 +0300

    less_6

---
+++ src/main/java/ru/runov/HwServlet.java
@@ -0,0 +1,36 @@
+package ru.runov;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/servl")
+public class HwServlet implements Servlet {
+    public transient ServletConfig sc;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.sc = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().println("<h1>Hello world !</h1>");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
